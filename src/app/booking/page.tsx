962093b9a5a8ad352b5f8c4034f49dd6bdf8f6b0
kevin <kevin.progc@gmail.com>
2025-11-06 19:42:38 +0100

    Fonction recherche de services page réservation

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -8,9 +8,11 @@ import { getAllCategories } from "../actions/categories";
 
 export default function Booking()
 {
-    const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState<any>([]);
     const [catSelected, setCatSelected] = useState<string>("Photographe professionnel");
+    const [lastCatSelected, setLastCatSelected] = useState<string>("Photographe professionnel");
     const [services, setServices] = useState(new Map());
+    const [servicesSearch, setServicesSearch] = useState<any[]>([]);
 
     useEffect(() =>
     {
@@ -33,6 +35,36 @@ export default function Booking()
     {
         //console.log(catId);
         setCatSelected(cat.name);
+        setLastCatSelected(cat.name);
+    }
+
+    const handleSearch = (input: string) =>
+    {
+        if(input !== "")
+        {
+            //console.log(input);
+            const servicesArray: any[] = [];
+
+            services.forEach((service) =>
+            {
+                service.map((item: any) =>
+                {
+                    servicesArray.push(item);
+                });
+            });
+
+            setCatSelected("");
+
+            const res = servicesArray.filter((item) => item.title.toLowerCase().indexOf(input.toLowerCase()) > -1 || item.description.toLowerCase().indexOf(input.toLowerCase()) > -1);
+            //console.log(res);
+
+            setServicesSearch(res);
+        }
+
+        else
+        {
+            setCatSelected(lastCatSelected);
+        }
     }
 
     return (
@@ -40,15 +72,15 @@ export default function Booking()
             <Navbar />
             
             <div>
-                <h1 className="my-3">BOOKLY PRO</h1>
-                <input type="text" placeholder="Trouver des services" className="mb-3" />
+                <h1 className="mt-3 mb-6 text-4xl text-bold">BOOKLY PRO</h1>
+                <input type="text" placeholder="Trouver des services ..." className="rounded-lg border-1 border-gray-300 w-[400px] h-[50px] mb-6" onChange={(e) => handleSearch(e.target.value)} />
 
-                <div className="flex justify-between items-center bg-gray-100 w-[770px] h-[50px] rounded-lg mb-3">
+                <div className="flex justify-between items-center bg-gray-100 w-[770px] h-[50px] rounded-lg mb-6">
                     {
                         categories.map((cat: any, index: number) =>
                         {
                             return (
-                                <button key={index} className={`rounded-md px-2 h-3/4 ${cat.name === catSelected ? "bg-white text-indigo-700 text-bold border-1 border-gray-200" : ""} hover:cursor-pointer`} onClick={() => handleSetCat(cat)}>{cat.name}</button>
+                                <button key={index} className={`rounded-lg px-2 h-9/10 ${cat.name === catSelected ? "bg-white text-indigo-700 text-bold border-1 border-gray-200" : ""} hover:cursor-pointer`} onClick={() => handleSetCat(cat)}>{cat.name}</button>
                             )
                         })
                     }
@@ -57,12 +89,34 @@ export default function Booking()
                 {
                     services.get(catSelected) &&
                     
-                    <div className="rounded-md border-1 border-gray-300 w-[400px]">
+                    <div className="rounded-lg border-1 border-gray-300 w-[400px]">
                         {
                             services.get(catSelected).map((service: any, index: number) =>
                             {
                                 return (
                                     <div key={index} className={`${index < services.get(catSelected).length - 1 ? "border-b-1" : ""} border-gray-300 w-9/10 mx-auto my-5`}>
+                                        <h1 className="text-xl">{service.title}</h1>
+                                        <div className="flex justify-between">
+                                            <h1 className="text-lg text-gray-600">{service.duration} min</h1>
+                                            <h1 className="text-lg text-gray-600">{service.price} €</h1>
+                                        </div>
+                                        <p className="text-lg text-gray-600 mb-2">{service.description}</p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                }
+
+                {
+                    !services.get(catSelected) &&
+                    
+                    <div className="rounded-lg border-1 border-gray-300 w-[400px]">
+                        {
+                            servicesSearch.map((service: any, index: number) =>
+                            {
+                                return (
+                                    <div key={index} className={`${index < servicesSearch.length - 1 ? "border-b-1" : ""} border-gray-300 w-9/10 mx-auto my-5`}>
                                         <h1 className="text-xl">{service.title}</h1>
                                         <div className="flex justify-between">
                                             <h1 className="text-lg text-gray-600">{service.duration} min</h1>
